d27312e55dced4e6ad907c617e2f1798c842b2d3
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2020-09-07 13:02:17 +0200

    Add files via upload

---
+++ test001.py
@@ -0,0 +1,35 @@
+# -*-coding:utf-8-*-
+
+import tkinter
+import tkinter.messagebox
+
+
+class Gui:
+    def mainProgram(self):
+        # define a button
+        def buttonClick():
+            tkinter.messagebox.showinfo("Hallo, welcome to my World!")
+        
+        # 创建窗体
+        window = tkinter.Tk()
+        
+        # creat a interface 400*300 resolution, width:400,height:300,the horizontal position 400, the vertical position 200
+        window.geometry("400x300+400+200")
+        
+        window.title("The world of Inkfish")
+        # first parameter: window, second: display word
+        # creat a label
+        label = tkinter.Label(window, text="Programming change the world!")
+        # pacl the label we created before
+        label.pack()
+        
+        # creat a button
+        btn = tkinter.Button(window, text="Surprise",command=buttonClick)
+        btn.pack()
+        
+        # strat mainloop
+        tkinter.mainloop()
+        
+
+main = Gui()
+main.mainProgram()
