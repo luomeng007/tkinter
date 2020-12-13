015628a31e3ce3094f82d2e7a2bb2d133d67f239
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2020-12-13 13:51:30 +0100

    Update and rename changeLabelText.py to changeLabelTextByEntry.py

--- basis/changeLabelTextByEntry.py
+++ basis/changeLabelTextByEntry.py
@@ -34,27 +34,3 @@ class MyGUI:
 
 
 myGUI = MyGUI()
-
-
-# ===============================================================================================================================
-# method to create child window widget, like here label, this is a very rare method!
-def changeLabel():
-    # create a child window of lb2
-    # because we do not store Label object into a variable when we create Label 'lb2'
-    # but the Label is on the root window, so we could create a child window from the root to save get the created Label
-    # 'lb2' by using its name. Then we could use lb2.configure to change the text on it.
-    lb2 = root.children['lb2']
-    # reconfigure children window, we could not see hello, but after three seconds, update text to world!
-    # method 1
-    lb2['text'] = 'world!'
-    # method 2
-    # configure is same as config
-    # lb2.configure(text='world!')
-
-
-root = Tk()
-# create a label named 'lb2'
-Label(name='lb2', text='hello').pack()
-Button(text='press to change', command=changeLabel).pack()
-
-root.mainloop()
