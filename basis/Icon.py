bef43a14c7704d8c85f92c7ca5c03d2f93c75941
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2020-12-13 12:49:36 +0100

    Add files via upload

---
+++ basis/Icon.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+"""
+author: 15025
+age: 26
+e-mail: 1502506285@qq.com
+time: 2020/12/13 12:46
+software: PyCharm
+
+Description:
+"""
+from tkinter import *
+
+# The icon
+root = Tk()
+# change the icon of tkinter window
+root.wm_iconbitmap('cat.ico')
+root.mainloop()
