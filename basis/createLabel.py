2cea45b2a11e3b1f6341e26d14283fa78bb309a7
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2021-11-15 08:11:53 +0100

    Update createLabel.py

--- basis/createLabel.py
+++ basis/createLabel.py
@@ -18,7 +18,7 @@ l = tkinter.Label(window,
                   bg='pink',  # 背景颜色
                   font=('Arial', 12),  # 字体和字体大小
                   width=15, height=2,  # 标签长宽
-                  )
+                  ) # 创建label时关键字text=不能省略
 # 固定窗口位置
 l.pack()
 
